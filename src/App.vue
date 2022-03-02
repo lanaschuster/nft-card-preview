c3ef01fea3a0152c11dc4a017e588f62301aa6ee
Lana Schuster <lanaschuster1@gmail.com>
2022-03-01 23:29:27 -0300

    feat: Nft card component layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main>
+    <nft-card></nft-card>
+  </main>
 </template>
 
+<script>
+import NftCard from './components/nft-card/nft-card.vue'
+
+export default {
+  components: {
+    NftCard
+  },
+  setup() {
+    
+  }
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  font-family: $font-family;
+}
+body, #app {
+  height: 100vh;
+  background-color: $main-bg-color;
+}
+main {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/nft-card/nft-card.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="card">
+    <img src="../../assets/images/image-equilibrium.jpg" alt="NFT image">
+    <div class="card-content">
+      <h3 class="card-title">Equilibrium #3429</h3>
+      <p class="card-paragraph">
+        Our Equilibrium collection promotes balance and calm.
+      </p>
+      <div class="card-values">
+        <div class="nft-value">
+          <img src="../../assets/images/icon-ethereum.svg" alt="NFT ethereum value">
+          0.041 ETH
+        </div>
+        <div class="nft-time">
+          <img src="../../assets/images/icon-clock.svg" alt="NFT time">
+          3 days left
+        </div>
+      </div>
+    </div>
+    <div class="card-footer">
+      <div class="nft-creator">
+        <img src="../../assets/images/image-avatar.png" alt="NFT Creator">
+        <p>Creation of <span>Jules Wyvern</span></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  width: 286px;
+  height: fit-content;
+  display: flex;
+  row-gap: 24px;
+  padding: 24px;
+  border-radius: 16px;
+  flex-direction: column;
+  background-color: $card-bg-color;
+
+  img {
+    width: 100%;
+    border-radius: 8px;
+  }
+
+  .card-content {
+    .card-title {
+      color: $white;
+      margin-bottom: 16px;
+    }
+
+    .card-paragraph {
+      color: $primary;
+      font-weight: 300;
+      font-size: $paragraph-font-size;
+    }
+
+    .card-values {
+      padding: 24px 0 0 0;
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: space-between;
+
+      .nft-value {
+        display: flex;
+        column-gap: 8px;
+        font-weight: 600;
+        color: $secondary;
+        align-items: center;
+        letter-spacing: 0.025rem;
+
+        img {
+          width: 12px;
+        }
+      }
+
+      .nft-time {
+        display: flex;
+        color: $primary;
+        column-gap: 8px;
+        font-weight: 300;
+        align-items: center;
+        letter-spacing: 0.025rem;
+
+        img {
+          width: 16px;
+        }
+      }
+
+    }
+  }
+
+  .card-footer {
+    height: fit-content;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      height: 1px;
+      width: 100%;
+      background-color: $primary;
+      margin-bottom: 16px;
+      opacity: 0.3;
+    }
+
+    .nft-creator {
+      display: flex;
+      align-items: center;
+      column-gap: 16px;
+      letter-spacing: 0.025rem;
+
+      img {
+        width: 32px;
+        border: 1px solid $white;
+        border-radius: 500px;
+      }
+
+      p {
+        color: $primary;
+        font-weight: 300;
+
+        span {
+          color: $white;
+          font-weight: 400;
+        }
+      }
+    }
+  }
+}
+</style>
